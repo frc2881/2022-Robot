0e194382dca696568cd2be069b0c91086ce3d322
Lady Cans <frc2881@gmail.com>
2022-02-25 20:43:58 -0600

    Added 45 second warning for climb time--the LEDs flash yellow

--- src/main/java/frc/robot/subsystems/PrettyLights.java
+++ src/main/java/frc/robot/subsystems/PrettyLights.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -12,6 +13,8 @@ public class PrettyLights extends SubsystemBase {
   public final double hotPink = 0.57;
   public final double green = 0.77;
   public final double red = 0.61;
+  public final double yellow = 0.69;
+  private boolean defaultColor = true;
   private final Spark lights;
 
   /** Creates a new PrettyLights. */
@@ -32,16 +35,26 @@ public class PrettyLights extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    if(defaultColor == true){
+        if(DriverStation.getMatchTime() <= 45 && DriverStation.getMatchTime() > 43){
+            lights.set(yellow);
+        } else {
+            lights.set(hotPink);
+        }
+    }
+
   }
 
 
   public void defaultColor(){
-    lights.set(hotPink);
+    defaultColor = true;
   }
   public void greenColor(){
+    defaultColor = false;
     lights.set(green);
   }
   public void redColor(){
+    defaultColor = false;
     lights.set(red);
   }
 
