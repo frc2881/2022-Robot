ffc7780bcefaa846f8fe4a964e65a2722c41cc21
Brian Kircher <bckircher@yahoo.com>
2022-02-27 21:27:29 -0600

    Simplify the CameraSwitch command.

--- src/main/java/frc/robot/commands/drive/CameraSwitch.java
+++ src/main/java/frc/robot/commands/drive/CameraSwitch.java
@@ -1,6 +1,7 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// Copyright (c) 2022 FRC Team 2881 - The Lady Cans
+//
+// Open Source Software; you can modify and/or share it under the terms of BSD
+// license file in the root directory of this project.
 
 package frc.robot.commands.drive;
 
@@ -10,36 +11,25 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class CameraSwitch extends CommandBase {
+  private final NetworkTableEntry m_cameraFront;
+
   /** Creates a new SwitchCamera. */
   public CameraSwitch() {
-    // Use addRequirements() here to declare subsystem dependencies.
+    NetworkTableInstance ntinst = NetworkTableInstance.getDefault();
+    NetworkTable table = ntinst.getTable("RPi");
+    m_cameraFront = table.getEntry("cameraFront");
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    NetworkTableInstance ntinst = NetworkTableInstance.getDefault();
-    NetworkTable table = ntinst.getTable("RPi");
-    NetworkTableEntry cameraFront = table .getEntry("cameraFront");
-    cameraFront.setBoolean(true);
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    NetworkTableInstance ntinst = NetworkTableInstance.getDefault();
-    NetworkTable table = ntinst.getTable("RPi");
-    NetworkTableEntry cameraFront = table.getEntry("cameraFront");
-    cameraFront.setBoolean(false);
+    m_cameraFront.setBoolean(false);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    NetworkTableInstance ntinst = NetworkTableInstance.getDefault();
-    NetworkTable table = ntinst.getTable("RPi");
-    NetworkTableEntry cameraFront = table .getEntry("cameraFront");
-    cameraFront.setBoolean(true);
+    m_cameraFront.setBoolean(true);
   }
 
   // Returns true when the command should end.
