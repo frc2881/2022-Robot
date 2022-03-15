8c2b9d6f541efb535cbaf215abbe00cde5a836ae
Lady Cans <frc2881@gmail.com>
2022-03-15 15:40:07 -0500

    Added pitch to lim function

--- src/main/java/frc/robot/subsystems/VisionTracking.java
+++ src/main/java/frc/robot/subsystems/VisionTracking.java
@@ -22,6 +22,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VisionTracking extends SubsystemBase {
 
+
   private PhotonCamera m_camera = new PhotonCamera("photonvision");
   /** Creates a new VisionTracking. */
   public VisionTracking() {}
@@ -53,22 +54,27 @@ public class VisionTracking extends SubsystemBase {
   
   }
 
-public double leftCatapultVision(double targetDist){
+public double CatapultDistToLim(double targetDist){
 
     double limit = ((targetDist - kLeftLowDist)/(kLeftHighDist - kLeftLowDist) * (kLeftHighLim - kForwardLimitLeft) + kForwardLimitLeft);
     return limit;
 }
 
-public double rightCatapultVision(double targetDist){
 
-  double limit = ((targetDist - kRightLowDist)/(kRightHighDist - kRightLowDist) * (kRightHighLim - kForwardLimitRight) + kForwardLimitRight);
-  return limit; 
+public void CatapultPitchToDist(){
+  
+  double pitch;
 
-}
+  if(m_camera.getLatestResult().getBestTarget() == null){
 
+    pitch = 0;
 
-public void CatapultDistance(CANSparkMax m_catapult){
-  double pitch = m_camera.getLatestResult().getBestTarget().getPitch();
+  }
+  else{
+
+    pitch = m_camera.getLatestResult().getBestTarget().getPitch();
+
+  }
 
   double lowerPitch=0;
   double lowerDist=0;
@@ -80,25 +86,73 @@ public void CatapultDistance(CANSparkMax m_catapult){
                         {9.0, -13.75},
                         {11.0, -18.1},
                         {12, -20.45}};
-  for(int i = 0; i < pitches.length-1; i++){
+  for(int i = 0; i <= pitches.length-1; i++){
     if(pitches[i][1] < pitch){
       lowerPitch = pitches[i][1];
       lowerDist = pitches[i][0];
-      higherPitch = pitches[i+1][1];
-      higherDist = pitches[i+1][0];
+      higherPitch = pitches[i-1][1];
+      higherDist = pitches[i-1][0];
+
+      break;
     }
 
   }
-  //this.leftCatapultVision(((pitch - lowerPitch)/(higherPitch - lowerPitch) * (higherDist - lowerDist) + lowerDist), m_catapult);
+  this.CatapultDistToLim(((pitch - lowerPitch)/(higherPitch - lowerPitch) * (higherDist - lowerDist) + lowerDist));
 }
 
-public double getYaw(){
+public double CatapultPitchToLim(){
 
-  double yaw = m_camera.getLatestResult().getBestTarget().getYaw();
+  double pitch;
 
   if(m_camera.getLatestResult().getBestTarget() == null){
 
-    yaw = 0; 
+    pitch = 0;
+
+  }
+  else{
+
+    pitch = m_camera.getLatestResult().getBestTarget().getPitch();
+
+  } 
+
+  double lowerPitch=0;
+  double lowerLim=0;
+  double higherPitch=0;
+  double higherLim=0;
+  double[][] pitches = {{4.5, 13.24},
+                        {4.9, 0.9},
+                        {5.3, -7.8},
+                        {5.8, -13.5},
+                        {6.9, -18.4},
+                        {8, -20.2}};
+  for(int i = 0; i <= pitches.length-1; i++){
+    if(pitches[i][1] < pitch){
+      lowerPitch = pitches[i][1];
+      lowerLim = pitches[i][0];
+      higherPitch = pitches[i-1][1];
+      higherLim = pitches[i-1][0];
+      
+      break;
+    }
+
+  }
+  double limit = (pitch - lowerPitch)/(higherPitch - lowerPitch) * (higherLim - lowerLim) + lowerLim;
+  return limit;
+}
+
+
+public double getYaw(){
+
+  double yaw;
+
+  if(m_camera.getLatestResult().getBestTarget() == null){
+
+    yaw = 0;
+
+  }
+  else{
+
+    yaw = m_camera.getLatestResult().getBestTarget().getYaw();
 
   }
 
